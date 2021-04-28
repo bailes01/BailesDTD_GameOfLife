c5c5351c2567a5719984a2a9ed02f6dfe777759d
bailes01 <bailey.chater9@gmail.com>
2021-04-28 20:14:15 +0930

    added inputs (need to label them)

--- js/grid.js
+++ js/grid.js
@@ -1,88 +1,104 @@
 class Grid {
-    constructor(cols, rows) {
-        this.iarr = [-1,-1,-1,0,0,+1,+1,+1];
-        this.jarr = [-1,0,+1,-1,+1,-1,0,+1];
-        this.cols = cols;
-        this.rows = rows;
-        this.cellWidth = width/cols;
-        this.cellHeight = height/rows;
-        this.cells = [];
-        for (var i = 0; i < cols; i++) {
-            var col = [];
-            for (var j = 0; j < rows; j++) {
-                // col.push(round(random()));
-                col.push(0);
-            }
-            this.cells.push(col);
-        }
+  constructor(cols, rows) {
+    this.iarr = [-1, -1, -1, 0, 0, +1, +1, +1];
+    this.jarr = [-1, 0, +1, -1, +1, -1, 0, +1];
+    this.cols = cols;
+    this.rows = rows;
+    this.cellWidth = width / cols;
+    this.cellHeight = height / rows;
+    this.cells = [];
+    for (var i = 0; i < cols; i++) {
+      var col = [];
+      for (var j = 0; j < rows; j++) {
+        // col.push(round(random()));
+        col.push(0);
+      }
+      this.cells.push(col);
+    }
   }
 
-    getNeighbours(i, j) {
-        var neighbours = [];
-        var neighbourSum = 0;
-        for(var k = 0 ; k < 8 ; k++){
-            var Iindex = i + this.iarr[k];
-            var Jindex = j + this.jarr[k];
-            if (Iindex >= 0 && Iindex < this.cols && Jindex >= 0 && Jindex < this.rows) {
-                neighbours.push(this.cells[Iindex][Jindex]);
-                neighbourSum += this.cells[Iindex][Jindex];
-            } else {
-                neighbours.push(null);
-            }
-        }
-        return neighbourSum;
+  getNeighbours(i, j) {
+    var neighbours = [];
+    var neighbourSum = 0;
+    for (var k = 0; k < 8; k++) {
+      var Iindex = i + this.iarr[k];
+      var Jindex = j + this.jarr[k];
+      // if (Iindex >= 0 && Iindex < this.cols && Jindex >= 0 && Jindex < this.rows) {
+      if (Iindex >= this.cols) {
+        Iindex = 0;
+      }
+      if (Jindex >= this.rows) {
+        Jindex = 0;
+      }
+      if (Iindex < 0) {
+        Iindex = this.cols - 1;
+      }
+      if (Jindex < 0) {
+        Jindex = this.rows - 1;
+      }
+      neighbours.push(this.cells[Iindex][Jindex]);
+      neighbourSum += this.cells[Iindex][Jindex];
     }
-    randomGrid() {
-        this.cells = [];
-        for (var i = 0; i < this.cols; i++) {
-            var col = [];
-            for (var j = 0; j < this.rows; j++) {
-                col.push(round(random()));
-            }
-            this.cells.push(col);
-        }
+    return neighbourSum;
+  }
+  randomGrid() {
+    this.cells = [];
+    for (var i = 0; i < this.cols; i++) {
+      var col = [];
+      for (var j = 0; j < this.rows; j++) {
+        col.push(round(random()));
+      }
+      this.cells.push(col);
     }
-    nextGen() {
-        var nextCells = [];
-        for (var i = 0; i < this.cols; i++) {
-            var nextRow = [];
-            for (var j = 0; j < this.rows; j++) {
-                var cell = this.cells[i][j];
-                var neighbours = this.getNeighbours(i, j);
-                if (cell == 1) {
-                    if (neighbours < 2 || neighbours > 3) {
-                        nextRow.push(0);
-                    } else {
-                        nextRow.push(1)
-                    }
-                } else if(neighbours == 3){
-                    nextRow.push(1);
-                } else {
-                    nextRow.push(0);
-                }
-            }
-            nextCells.push(nextRow);
-        }
-        this.cells = nextCells;
-    }
-    render() {
-        if (paused) {
-            stroke(50);
+  }
+  nextGen() {
+    var nextCells = [];
+    for (var i = 0; i < this.cols; i++) {
+      var nextRow = [];
+      for (var j = 0; j < this.rows; j++) {
+        var cell = this.cells[i][j];
+        var neighbours = this.getNeighbours(i, j);
+        if (cell == 1) {
+          if (neighbours < 2 || neighbours > 3) {
+            nextRow.push(0);
+          } else {
+            nextRow.push(1);
+          }
+        } else if (neighbours == 3) {
+          nextRow.push(1);
         } else {
-            stroke(20);
+          nextRow.push(0);
         }
-        for (var i = 0; i < this.cells.length; i++) {
-            for (var j = 0; j < this.cells[i].length; j++){
-                if (this.cells[i][j] == 0) {
-                    fill(0);
-                } else {
-                    fill(200,0,200);
-                }
-                rect(i * this.cellWidth, j * this.cellHeight, this.cellWidth, this.cellHeight);
-                // fill(255);
-                // text(this.getNeighbours(i, j).toString(), i * this.cellWidth + this.cellWidth/2, j * this.cellHeight + this.cellHeight/2);
-            }
-            
-        }
+      }
+      nextCells.push(nextRow);
     }
+    this.cells = nextCells;
+  }
+  render() {
+    if (paused) {
+      stroke(50);
+    } else {
+      stroke(20);
+    }
+    if (this.cols > width / 4) {
+      noStroke();
+    }
+    for (var i = 0; i < this.cells.length; i++) {
+      for (var j = 0; j < this.cells[i].length; j++) {
+        if (this.cells[i][j] == 0) {
+          fill(0);
+        } else {
+          fill(200, 0, 200);
+        }
+        rect(
+          i * this.cellWidth,
+          j * this.cellHeight,
+          this.cellWidth,
+          this.cellHeight
+        );
+        // fill(255);
+        // text(this.getNeighbours(i, j).toString(), i * this.cellWidth + this.cellWidth/2, j * this.cellHeight + this.cellHeight/2);
+      }
+    }
+  }
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,61 +1,108 @@
 ///<reference path='../p5.global-mode.d.ts'/>
 var paused = true;
+var started = false;
+
 var grid;
-
+var newGridButton;
+var gridSizeSlider;
+var frameRateSlider;
 var body = document.querySelector("body");
-
 var canvasContainer = document.getElementById("canvas-container");
-var margin = parseInt(window.getComputedStyle(canvasContainer).getPropertyValue("padding").replace("px", ""));
-function setup() 
-{
-	var gridSize = windowHeight - margin * 3
-	console.log(margin)
-	var myCanvas = createCanvas(gridSize, gridSize);
-	canvasContainer.style.height = gridSize + "px";
-	canvasContainer.style.width = gridSize + "px";
-	myCanvas.parent(canvasContainer)
-	frameRate(5);
-	grid = new Grid(50, 50);
-	grid.render();
+var margin = parseInt(
+  window
+    .getComputedStyle(canvasContainer)
+    .getPropertyValue("padding")
+    .replace("px", "")
+);
+var panel = document.getElementById("panel");
+
+var gridSize;
+var myCanvas;
+var cellCount = 50;
+
+function setup() {
+  gridSize = windowHeight - margin * 3;
+  canvasContainer.style.height = gridSize + "px";
+  canvasContainer.style.width = gridSize + "px";
+
+  myCanvas = createCanvas(gridSize, gridSize);
+  myCanvas.parent(canvasContainer);
+  myCanvas.mouseClicked(canvasClicked);
+
+  pauseButton = createButton("Start (SpaceBar)");
+  pauseButton.parent(panel);
+  pauseButton.mouseReleased(togglePause);
+
+  cellCountSlider = createSlider(10, gridSize, 50, 1);
+  cellCountSlider.parent(panel);
+  cellCountSlider.style("width", "80px");
+  newGridButton = createButton("New Grid (N)");
+  newGridButton.parent(panel);
+  newGridButton.mousePressed(createNewGrid);
+  frameRateSlider = createSlider(1, 30, 10, 1);
+  frameRateSlider.parent(panel);
+  frameRateSlider.style("width", "80px");
+
+  createNewGrid();
+  frameRate(frameRateSlider.value());
 }
 
-function draw()
-{
-	if (!paused) {
-		background(0);
-		grid.nextGen();
-		
-		}
-	grid.render();
+function togglePause() {
+  started = true;
+  paused = !paused;
+  console.log(paused);
+}
+function createNewGrid() {
+  paused = true;
+  started = false;
+  cellCount = cellCountSlider.value();
+  grid = new Grid(cellCount, cellCount);
+  grid.render();
+}
+
+function draw() {
+  frameRate(frameRateSlider.value());
+  if (started) {
+    if (!paused) {
+      pauseButton.html("Pause (SpaceBar)");
+      background(0);
+      grid.nextGen();
+    } else {
+      pauseButton.html("Resume");
+    }
+    grid.render();
+  } else {
+    pauseButton.html("Start (SpaceBar)");
+  }
 }
 
 function keyPressed() {
-	if (key == " ") {
-		paused = !paused;
-	}
-	console.log(paused);
-	if (key == "r") {
-		console.log("R")
-		if (!paused) {
-			paused = !paused;
-		}
-		grid.randomGrid();
-	}
+  if (key == " ") {
+    togglePause();
+  }
 
+  if (key == "r") {
+    paused = true;
+    grid.randomGrid();
+    grid.render();
+  }
+
+  if (key == "n") {
+    createNewGrid();
+  }
 }
 
-function mouseClicked() {
-	if (paused) {
-		var i = floor(mouseX / (width / grid.cells.length));
-		var j = floor(mouseY / (height / grid.cells.length));
-		if (grid.cells[i][j] == 0) {
-			grid.cells[i][j] = 1;
-		} else {
-			grid.cells[i][j] = 0;
-		}
-		grid.render();
-	}
-	
+function canvasClicked() {
+  if (paused) {
+    var i = floor(mouseX / (width / grid.cells.length));
+    var j = floor(mouseY / (height / grid.cells.length));
+    if (grid.cells[i][j] == 0) {
+      grid.cells[i][j] = 1;
+    } else {
+      grid.cells[i][j] = 0;
+    }
+  } else {
+    paused = true;
+  }
+  grid.render();
 }
-
-
